0b44bd564bf8ecab82e638da6f3ed3e5e75885fd
od <kk26181888@gmail.com>
2026-09-13 18:19:58 +0800

    Remove database loader, add Wellcome promotional banners

--- src/main/java/com/discount/discount_web/DiscountWebApplication.java
+++ src/main/java/com/discount/discount_web/DiscountWebApplication.java
@@ -1,4 +1,4 @@
-package com.discount.discount_web;
+git checkout mainpackage com.discount.discount_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
