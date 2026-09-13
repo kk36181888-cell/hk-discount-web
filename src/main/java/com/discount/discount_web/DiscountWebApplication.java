2967f80ee11d7d8e99631db5a29ba9874caf9be8
od <kk26181888@gmail.com>
2026-09-13 18:22:15 +0800

    Fix syntax error in DiscountWebApplication.java

--- src/main/java/com/discount/discount_web/DiscountWebApplication.java
+++ src/main/java/com/discount/discount_web/DiscountWebApplication.java
@@ -1,4 +1,4 @@
-git checkout mainpackage com.discount.discount_web;
+package com.discount.discount_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiscountWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscountWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountWebApplication.class, args);
+    }
 
 }
